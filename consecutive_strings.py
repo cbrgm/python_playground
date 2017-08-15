550e394f07866d2d7c76a8748abe86efa5d1a0c7
Bargmann <Bargmann@hmc.de>
2017-08-15 14:17:26 +0200

    Added new Katas

---
+++ consecutive_strings.py
@@ -0,0 +1,14 @@
+# You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+# Example: longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+# n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
+
+def longest_consec(strarr, k):
+    result = ""
+    
+    if k > 0 and len(strarr) >= k:
+        for index in range(len(strarr) - k + 1):
+            s = ''.join(strarr[index:index+k])
+            if len(s) > len(result):
+                result = s
+            
+    return result
